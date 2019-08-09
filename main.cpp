1948e3966adeb3e3c53ebb784cdb746d10ee43ae
Leon Latsch <leonlatsch@gmx.de>
2019-08-09 14:50:57 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// main.cpp / This file contains the main() method
+
+#include <iostream>
+#define MULTI_LINE_STRING(a)
+
+using namespace std;
+
+void greeting()
+{
+    const char *line1 = "/ ___|   / ___|  _ __   _   _   _ __   | |_ \n";
+    const char *line2 = "\\___ \\  | |     | '__| | | | | | '_ \\  | __|\n";
+    const char *line3 = " ___) | | |___  | |    | |_| | | |_) | | |_ \n";
+    const char *line4 = "|____/   \\____| |_|     \\__, | | .__/   \\__|\n";
+    const char *line5 = "                        |___/  |_|          \n";
+
+    cout << line1;
+    cout << line2;
+    cout << line3;
+    cout << line4;
+    cout << line5;
+}
+
+int main(int argc, char** argv)
+{
+    greeting();
+}
